38bc134b7e51de6345764ddfa22a2a4ddfda3d08
stv-beep <aleixalguero0@gmail.com>
2022-11-16 15:12:59 +0100

    leap years

--- Python/26leapYear.py
+++ Python/26leapYear.py
@@ -1,4 +1,4 @@
-# 25. - mouredev.weeklychallenge2022 #25
+# 31. - mouredev.weeklychallenge2022 #31
 """ Crea una función que imprima los 30 próximos años bisiestos siguientes a uno dado.
 - Utiliza el menor número de líneas para resolver el ejercicio. """
 
